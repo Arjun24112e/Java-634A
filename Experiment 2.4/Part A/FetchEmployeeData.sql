875bd7799704182deb79edc323daefa12e7ac5c2
Arjun Dalal <23bcs10638@cuchd.in>
2025-11-04 22:27:42 +0530

    Delete Experiment 2.4 directory

--- Experiment 2.4/Part A/FetchEmployeeData.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Employee (
-    EmpID INT PRIMARY KEY,
-    Name VARCHAR(50),
-    Salary DOUBLE
-);
-INSERT INTO Employee VALUES (101, 'Lakshita', 60000),
-                            (102, 'Nitin', 55000),
-                            (103, 'Asha', 50000);
